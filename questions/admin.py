b84246a98ac17cb3d8952f6fb9679b9ae6a48b7f
silviotassini <silviotassini@gmail.com>
2022-06-22 07:28:31 -0300

    update

--- questions/admin.py
+++ questions/admin.py
@@ -3,9 +3,13 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Theme)
-admin.site.register(Question)
+
 admin.site.register(Answer)
 admin.site.register(Note)
 admin.site.register(QuestionTag)
 admin.site.register(Tag)
 
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['theme','asktext']
+    list_display=['theme','asktext','date_created']
+admin.site.register(Question, QuestionAdmin)
--- questions/forms.py
+++ questions/forms.py
@@ -17,7 +17,18 @@ class QuestionsForm(ModelForm):
         model = Question
         fields = "__all__"
 
+    def __init__(self, *args, **kwargs):
+        super(QuestionsForm, self).__init__(*args, **kwargs) # Call to ModelForm constructor
+        self.fields['asktext'].widget.attrs['cols'] = 120
+        self.fields['asktext'].widget.attrs['rows'] = 10 
+        self.fields['asksource'].widget.attrs['size'] = 100   
+
 class AnswersForm(ModelForm):
     class Meta():
         model = Answer
-        fields = "__all__"        
+        fields = "__all__" 
+
+    def __init__(self, *args, **kwargs):
+        super(AnswersForm, self).__init__(*args, **kwargs) # Call to ModelForm constructor
+        self.fields['answertext'].widget.attrs['size'] = 120
+        
